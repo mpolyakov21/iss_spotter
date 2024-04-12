3ee0a104648686c9b58f2aa5162c498002f5ac1a
maksym <maksym.polyakov@yahoo.ca>
2024-04-11 22:27:29 -0500

    Add error handling

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// The code below is temporary and can be commented out.
-const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
